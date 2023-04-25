afd7e077e9f429033e3c05d2d80c11e741dbf88f
Marine Pouligny <marinepouligny@gmail.com>
2023-04-25 11:45:38 +0200

    Ajout de la fonction filter

--- FrontEnd/home.js
+++ FrontEnd/home.js
@@ -11,7 +11,7 @@
          galerieProjet.innerHTML = ''
 
 //Ajout de chaque élément de la galerie 
-         jsonListeProjets.forEach(projet => {
+         jsonListeProjets.map(projet => {
              const figure = document.createElement('figure')
              const img = document.createElement('img')
              const figcaption = document.createElement('figcaption')
@@ -33,63 +33,25 @@ fetch ('http://localhost:5678/api/works')
 
         console.log(jsonListeProjets)
 
-        jsonListeProjets.forEach( afficherProjet => {
-//Je vais chercher le nom de mes filtres
-            let projetTableau = Object.values(jsonListeProjets)
-
-            let tableau = Object.values(afficherProjet)
-
-            let tableauBis = Object.values(tableau[5])
-            console.log(tableauBis[1])
+        const nomFiltre = jsonListeProjets.filter((projet, index)=> {
+            return console.log(projet.category.name)  
+        })
 
 //je relie JS au filtres HTML et suppression button HTML
-            let filtreGalerieProjet = document.querySelector('.filtres')
-            filtreGalerieProjet.innerHTML = '' 
+            const filtreGalerieProjet = document.querySelector('.filtres')
+            filtreGalerieProjet.innerHTML = ''    
 
 // j'ajoute les filtres
-            for (let i in projetTableau) {
-                let boutonFiltre = document.createElement('button')
-                boutonFiltre.textContent = tableauBis[1]
-                filtreGalerieProjet.appendChild(boutonFiltre)
-            }
-
-        })
             
-            
-
-
-         
-
-// Je vais chercher le nom du filtre
-/*for (let i=0; i<jsonListeProjets.lenght; i++){
-
-       jsonListeProjets[i] = new Array[i]
-       console.log(jsonListeProjets[i])
-        }*/
-    })    
-        //console.log(tableauBis[5])
-    
-        /*let tableauTer = Object.values(tableauBis[5])
-        console.log(tableauTer[1])
+            const boutonFiltre = document.createElement('button')
+            boutonFiltre.textContent = jsonListeProjets.nomFiltre
+            filtreGalerieProjet.appendChild(boutonFiltre)
         
-// relier JS au filtres HTML et suppression button HTML
-        const filtreGalerieProjet = document.querySelector('.filtres')
-        filtreGalerieProjet.innerHTML = '' 
-
-// ajout des filtres
-        const boutonFiltre = document.createElement('button')
-        boutonFiltre.textContent = tableauTer[1]
-        filtreGalerieProjet.appendChild(boutonFiltre)
-        }    
-    })
-    
+    })       
 
 // TEST
 /*fetch ('http://localhost:5678/api/works')
     .then(dataListeProjets => dataListeProjets.json())
     .then(jsonListeProjets => {
         console.log(jsonListeProjets)
-        let tableau = Object.values()
-            for (let i=0; i< jsonListeProjets.lenght; i++){
-            console.log(jsonListeProjets[i])
-            }*/
+       
